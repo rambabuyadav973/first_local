631be31c17e0ed8f4c3b1cb39a18259ef8b9425c
rambabuyadav973 <133943619+rambabuyadav973@users.noreply.github.com>
2023-05-21 18:39:14 +0530

    Update stringEvenOdd.java
    updated

--- stringEvenOdd.java
+++ stringEvenOdd.java
@@ -6,19 +6,6 @@ public class stringEvenOdd {
 	public static void main(String[] args) {
 		String str;
 		
-		Scanner sc = new Scanner(System.in);
-		
-		//enter the string
-		System.out.println("Enter the string");
-		str = sc.next();
-		
-		
-		//checking if length is even print half of string otherwise print null
-		if(str.length() % 2 == 0) {
-			System.out.println(str.substring(0, str.length()/2));
-		}else {
-			System.out.println("null");
-		}
-		
+		System.out.print("HEllO PBL!!");
 		}
 }
